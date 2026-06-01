bbf72ce2372d8d6aff7cf406b376d53b759da7d0
8RiVeR8 <maks.rzeczycki@gmail.com>
2026-06-01 20:44:42 +0200

    Initial commit

---
+++ src/test/java/com/project/youtlix/YoutlixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.youtlix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutlixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
