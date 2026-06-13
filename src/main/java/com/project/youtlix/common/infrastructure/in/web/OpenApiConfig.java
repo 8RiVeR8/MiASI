25121c543aec2170f7e6471e33e67d8920c9a76b
RP272 <272931@student.pwr.edu.pl>
2026-06-13 12:22:01 +0200

    feat: add Bearer JWT authorization to Swagger UI

--- src/main/java/com/project/youtlix/common/infrastructure/in/web/OpenApiConfig.java
+++ src/main/java/com/project/youtlix/common/infrastructure/in/web/OpenApiConfig.java
@@ -1,19 +1,30 @@
 package com.project.youtlix.common.infrastructure.in.web;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
 
+    public static final String BEARER_AUTH = "bearerAuth";
+
     @Bean
     public OpenAPI youtlixOpenApi() {
         return new OpenAPI()
                 .info(new Info()
                         .title("Youtlix API")
-                        .description("REST API for the Youtlix modular monolith")
-                        .version("0.0.1"));
+                        .description("REST API for the Youtlix modular monolith. "
+                                + "Use Authorize and paste the access token from /login or /register.")
+                        .version("0.0.1"))
+                .components(new Components()
+                        .addSecuritySchemes(BEARER_AUTH, new SecurityScheme()
+                                .name(BEARER_AUTH)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
     }
 }
